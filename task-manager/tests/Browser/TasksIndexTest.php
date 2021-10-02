c0c6e97962ce55f00a0679bd7165fa5b16804ee8
wakana <greentea.mmlg.0405@gmail.com>
2021-10-02 21:59:36 +0900

    第二章完了_インデックスから詳細に遷移

--- task-manager/tests/Browser/TasksIndexTest.php
+++ task-manager/tests/Browser/TasksIndexTest.php
@@ -22,4 +22,18 @@ class TasksIndexTest extends DuskTestCase
                 ->screenshot("tasks_index");
         });
     }
+
+    /**
+     * インデックスから詳細に飛ぶテスト
+     */
+    public function testIndexToDetail() {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/tasks')
+                ->assertSeeLink('テストタスク')
+                ->clickLink('テストタスク')
+                ->waitForLocation('/tasks/2', 1)
+                ->assertPathIs('/tasks/2')
+                ->assertSee('テストタスク');
+        });
+    }
 }
